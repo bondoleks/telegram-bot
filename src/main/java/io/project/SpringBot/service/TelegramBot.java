fd214413955814421128a285f217129afa25187f
Bondarchuk_Oleksandr <sbv121121@gmail.com>
2023-03-13 13:41:47 +0200

    Command list

--- src/main/java/io/project/SpringBot/service/TelegramBot.java
+++ src/main/java/io/project/SpringBot/service/TelegramBot.java
@@ -4,17 +4,36 @@ import io.project.SpringBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
 
+    static final String HELP_TEXT = "Demo test";
+
     public TelegramBot(BotConfig config){
         this.config = config;
+        List<BotCommand> listOfCommand = new ArrayList<>();
+        listOfCommand.add(new BotCommand("/start", "Welcome message"));
+        listOfCommand.add(new BotCommand("/myData", "Show data"));
+        listOfCommand.add(new BotCommand("/deleteData", "Delete my data"));
+        listOfCommand.add(new BotCommand("/help", "Help info"));
+        try{
+            this.execute(new SetMyCommands(listOfCommand, new BotCommandScopeDefault(), ""));
+        } catch (TelegramApiException e){
+            log.error("Error setting bot's commad list: " + e.getMessage());
+        }
     }
 
     @Override
@@ -35,6 +54,9 @@ public class TelegramBot extends TelegramLongPollingBot {
             switch (messageText){
                 case "/start":
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
                     break;
                 default:
                     sendMessage(chatId, "Default method");
