7f9a0c146c2ff6406b5a3baa163658946644819c
Bondarchuk_Oleksandr <sbv121121@gmail.com>
2023-03-15 21:37:52 +0200

    Added a newsletter to all users

--- src/main/java/io/project/SpringBot/model/User.java
+++ src/main/java/io/project/SpringBot/model/User.java
@@ -1,8 +1,6 @@
 package io.project.SpringBot.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.sql.Timestamp;
